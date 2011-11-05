f85119fdab17bc95c7f1ca5ccc87936bfc3cb73b
Xiong Changnian <xiong@cpan.org>
2011-11-04 23:07:07 -0700

    Bump version			v0.1.0

--- lib/Error/Base.pm
+++ lib/Error/Base.pm
@@ -9,7 +9,7 @@ package Error::Base;
 use 5.008008;
 use strict;
 use warnings;
-use version 0.77; our $VERSION = qv('v0.0.4');
+use version 0.77; our $VERSION = qv('v0.1.0');
 
 # Core modules
 use overload                    # Overload Perl operations
@@ -626,7 +626,7 @@ Error::Base - Simple structured errors with full backtrace
 
 =head1 VERSION
 
-This document describes Error::Base version v0.0.4
+This document describes Error::Base version v0.1.0
 
 =head1 SYNOPSIS
 
